db6c6babe0fb7e97d7edbcd7b9324bcdc188e27c
Jeyhun Jeyhunzade <ceyhun.zade1998@gmail.com>
2020-03-27 14:29:57 +0400

    Updates

---
+++ precache-manifest.217a944ff65fda6d2dd72e7bf02e9ab1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a831d664900918808597ff0f11b6d592",
+    "url": "/MapspotAP/index.html"
+  },
+  {
+    "revision": "1ffc6559cd8151da0ae6",
+    "url": "/MapspotAP/static/css/main.4b09a7c6.chunk.css"
+  },
+  {
+    "revision": "ad54db89b7856233f07b",
+    "url": "/MapspotAP/static/js/2.7828dc61.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MapspotAP/static/js/2.7828dc61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffc6559cd8151da0ae6",
+    "url": "/MapspotAP/static/js/main.5d04fe24.chunk.js"
+  },
+  {
+    "revision": "3e53df23f96d70b2e624",
+    "url": "/MapspotAP/static/js/runtime-main.06cd8abd.js"
+  },
+  {
+    "revision": "2140b11d162f7a365038ca80262a617a",
+    "url": "/MapspotAP/static/media/admin.2140b11d.jpg"
+  },
+  {
+    "revision": "9c2b662b14b01cfe1e19c1acd7bb0d4b",
+    "url": "/MapspotAP/static/media/map-pointer.9c2b662b.svg"
+  }
+]);
